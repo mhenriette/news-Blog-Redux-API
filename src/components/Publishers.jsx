b918f140c0b39f8bcfb14ae62c39c800baecc13a
mhenriette <mhenriette00@gnmail.com>
2023-04-11 13:30:40 +0200

    refactor: format code

--- src/components/Publishers.jsx
+++ src/components/Publishers.jsx
@@ -2,26 +2,26 @@ import { useGetPublisherQuery } from "../features/newsSlice";
 import PublisherCard from "./ui/PublisherCard";
 import Wrapper from "./wrappers/Wrapper";
 const Publishers = () => {
-    const { data, isSuccess } = useGetPublisherQuery();
+  const { data, isSuccess } = useGetPublisherQuery();
 
-    return (
-        <Wrapper>
-            <div className="my-10 mx-4">
-                {isSuccess && (
-                    <div>
-                        <h1 className="font-bold flex text-3xl truncate justify-between items-center flex-nowrap text-red-700 my-10 lg:after:w-4/5 lg:after:h-1 lg:after:block after:bg-red-700 ">
-                            Publishers
-                        </h1>
-                        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3 ">
-                            {data.map(({ name, id, uniqueId }) => (
-                                <PublisherCard name={name} id={id} key={uniqueId} />
-                            ))}
-                        </div>
-                    </div>
-                )}
+  return (
+    <Wrapper>
+      <div className="my-10 mx-4">
+        {isSuccess && (
+          <div>
+            <h1 className="font-bold flex text-3xl truncate justify-between items-center flex-nowrap text-red-700 my-10 lg:after:w-4/5 lg:after:h-1 lg:after:block after:bg-red-700 ">
+              Publishers
+            </h1>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3 ">
+              {data.map(({ name, id, uniqueId }) => (
+                <PublisherCard name={name} id={id} key={uniqueId} />
+              ))}
             </div>
-        </Wrapper>
-    );
+          </div>
+        )}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Publishers;
